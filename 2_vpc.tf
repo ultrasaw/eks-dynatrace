70d9641b6227b3498886f01186363ae3cd41d3aa
Georgy Charkseliani <giordano2407@gmail.com>
2025-02-14 19:38:21 +0100

    delete the ALB defined via Terraform and rely on the LB provisioned by ingress-nginx

--- 2_vpc.tf
+++ 2_vpc.tf
@@ -48,12 +48,12 @@ resource "aws_security_group" "allow_ssh_and_ports" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ingress {
-  #   from_port   = 80
-  #   to_port     = 443
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  ingress {
+    from_port   = 80
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   # dynatrace ports
   ingress {
---
+++ 3_eks.tf
@@ -0,0 +1,30 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.33.1"
+
+  cluster_name    = var.eks_cluster_name
+  cluster_version = var.eks_version
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id = module.vpc.vpc_id
+  # subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnets # beware that the worker nodes are exposed to the internet
+
+  # enable_irsa = true # use pod identity for eks instead
+
+  eks_managed_node_groups = {
+    mini-pool = {
+
+      desired_size = 1
+      min_size     = 1
+      max_size     = 1
+
+      instance_types = ["t3.xlarge"]
+      capacity_type  = "ON_DEMAND" # can be ON_DEMAND / SPOT
+    }
+  }
+
+  enable_cluster_creator_admin_permissions = true
+}
